9006f65044827f770ab5feafacec21fd62a7f5e5
yarshaqa <yarshaqa@gmail.com>
2021-03-22 10:11:16 +0200

    SixD is ready

--- Lesson2/SixD.java
+++ Lesson2/SixD.java
@@ -1,5 +1,27 @@
 public class SixD {
 
+    public static void main(String[] args) {
 
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j<4-i; j++){
+                System.out.print(" ");
+            }
+            for (int q = 0; q <(2*i+1); q++) {
+                System.out.print("*");
 
+            }
+            System.out.println();
+            }
+    }
+
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 8 - i; j++){
+//                System.out.print(" ");}
+//            for (int k = 0; k < (2 * i +1); k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
 }
